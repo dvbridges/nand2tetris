8e88623d8451ef8ba863232bbbe40c4356f88b4b
dvbridges <david-bridges@hotmail.co.uk>
2020-02-07 22:43:47 +0000

    Project 4 complete with mult and fill assembly code

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,75 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@8192
+D = A 
+@pix
+M = D           // Get pixel limit
+
+@SCREEN         // Get screen base address
+D = A 
+@screenBase
+M = D
+
+( LOOP )
+    
+    @KBD            // Get keyboard state
+    D = M 
+
+    @WIPE           // Wipe screen on no press
+    D; JEQ
+
+    @PAINT          // Paint on keypress
+    D; JGT
+
+( WIPE )
+
+    @8192
+    D = A 
+
+    @pix
+    M = D           // Reset pixel counter
+
+    @screenBase
+    D = M
+
+    @n 
+    A = D + M       // Set RAM address to screen base + n
+    M = 0
+
+    @n 
+    D = M 
+
+    @LOOP
+    D; JEQ          // If n is zero, back to loop
+
+    @n 
+    M = M - 1       // Else, decrease n
+
+    @LOOP
+    0; JMP          // Back to Loop
+
+( PAINT )
+
+    @pix            
+    D = M
+
+    @LOOP
+    D; JLE          // Return if we drew all pixels
+    
+    @pix
+    M = D - 1
+
+    @n 
+    D = M 
+
+    @screenBase
+    A = M + D       // Set address to screen base + n
+    M = -1          // Set screen to black
+
+    @n 
+    M = D + 1       // Increment n by 1
+
+    @LOOP
+    0; JMP          // Back to loop
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Take one of the values for the number of loops: n
+// Take one of the values for adding to the sum: val
+// Create a sum variable for the sum of all vals: sum
+
+
+@R0
+D = M
+@n
+M = D           // n = R0
+@R1
+D = M 
+@val
+M = D           // val = R1
+@sum
+M = 0
+@R2
+M = 0           // Reset output value
+
+( LOOP )
+    @sum
+    D = M       // Put sum in data register
+    @val
+    D = D + M   // Add val to sum
+    @sum
+    M = D       // Set sum in memory     
+    @n 
+    D = M - 1   // Decrease n by 1
+    M = D       // Set n in memory
+    @STOP
+    D; JEQ      // goto STOP if n == 0
+    @LOOP
+    0; JMP      // Restart Loop
+
+( STOP )
+    @sum
+    D = M       // Get sum
+    @R2
+    M = D       // Set R2 to sum val
+
+( END )
+    @END
+    0;JMP 
